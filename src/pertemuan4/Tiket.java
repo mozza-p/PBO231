cdb9d55ec861b0c35f9f06c2c65a8be92185b019
mozza-p <146147827+mozza-p@users.noreply.github.com>
2023-10-24 20:07:17 +0700

    Update Tiket.java

--- src/pertemuan4/Tiket.java
+++ src/pertemuan4/Tiket.java
@@ -2,19 +2,49 @@ package pertemuan4;
 
 public class Tiket {
     private String nama, noTelp, nmHotel, nmPesawat, tujuan;
-    private int jmlKamar, hrgKamar, jmlPenumpang, hrgPesawat;
-    public Tiket(String nama, String telp, String nmHotel,
-                 int jmlKamar, int hrgKamar, int nmPesawat,
-                 int jmlPenumpang, int hrgPesawat, String tujuan)
-    {
+    private int jmlPenumpang, hrgPesawat, hrgTiket, totalPesawat;
+    private double jmlKamar, hrgKamar, diskon, totalHotel;
+        public Tiket(String nama, String telp, String tujuan) {
         this.nama = nama;
         noTelp = telp;
-        this.nmHotel = nmHotel;
+        this.tujuan = tujuan;
+        }
+        public Tiket(String nmHotel) {
+            this.nmHotel = nmHotel;
+        }
+        public Tiket (double jmlKamar, double hrgKamar){
         this.jmlKamar = jmlKamar;
         this.hrgKamar = hrgKamar;
-        this
-
-    }
-    public Tiket()
+        }
+        public Tiket(String nmPesawat, int jmlPenumpang, int hrgPesawat){
+        this.nmPesawat = nmPesawat;
+        this.jmlPenumpang = jmlPenumpang;
+        this.hrgPesawat = hrgPesawat;
+        }
+        public String getNama(){return this.nama;}
+        public String getTelp(){
+        return this.noTelp;
+        }
+        public String getNmHotel(){
+        return this.nmHotel;
+        }
+         public double getjmlKamar(){return this.jmlKamar;}
+        public double getHrgKamar(){
+        return this.hrgKamar;
+        }
+        public String getNmPesawat(){return this.nmPesawat;}
+        public int getjmlPenumpang(){
+        return this.jmlPenumpang;
+         }
+        public int getHrgPesawat(){
+        return this.hrgPesawat;
+        }
+        public String getTujuan(){return this.tujuan;}
+        public int getTiket(){return getHrgPesawat() * getjmlPenumpang();}
+        public int getTotalPesawat(){return getTiket() + (getjmlPenumpang() * 29000);}
+        public double getDiskon(){return getHrgKamar() * 0.45;}
+        public double getTotalHotel(){return(getHrgKamar() - getDiskon()) * getjmlKamar();}
 
 }
+
+
